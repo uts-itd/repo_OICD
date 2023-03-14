122f828b5c1ca4d568e65afcae2ad043ff4dbead
Arshi-123 <122540756+Arshi-123@users.noreply.github.com>
2023-03-15 10:27:34 +1100

    Create back.tf

---
+++ back.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket = "kian-intern-teaching-state-bucket"
+    key    = "terraform.tfstate"
+    region = "ap-southeast-2"
+    dynamodb_table = "terraform-locks"
+  }
+  
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.34.0"
+    }
+  }
+}
