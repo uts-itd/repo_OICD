6ac5aae6b9e52a60a5c1befb995b92ba817875bf
Arshi Chariyekkara <arshi.chariyekkara@uts.edu.au>
2023-03-15 15:52:09 +1100

    update on providers

--- providers.tf
+++ providers.tf
@@ -1,4 +1,11 @@
-
-provider "aws" {
-  region = "ap-southeast-2"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.57.0"
+    }
+  }
+} provider "aws" {
+    region = "ap-southeast-2"
+    profile = "AWSPowerUserAccess-046955552049"
 }
